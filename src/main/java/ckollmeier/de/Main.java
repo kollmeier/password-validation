34399dd432cdaf9ba4fb6c57c5b3ea0e27bb7b6c
Carsten Kollmeier <carsten@ckollmeier.de>
2025-03-31 15:51:51 +0200

    add some interaction

--- src/main/java/ckollmeier/de/Main.java
+++ src/main/java/ckollmeier/de/Main.java
@@ -1,7 +1,22 @@
 package ckollmeier.de;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Starting here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your new password: ");
+        String password = scanner.nextLine();
+        System.out.println("Please confirm your new password: ");
+        String confirmPassword = scanner.nextLine();
+        if (!password.equals(confirmPassword)) {
+            System.out.println("Passwords do not match");
+            return;
+        }
+        if (!PasswordValidation.passwordIsValid(password)) {
+            System.out.println("Password is invalid");
+            return;
+        }
+        System.out.println("Password valid");
     }
 }
