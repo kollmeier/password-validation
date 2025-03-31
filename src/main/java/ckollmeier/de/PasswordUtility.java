b45b5e308f9655e64044fa56868352b8fb60620f
Carsten Kollmeier <carsten@ckollmeier.de>
2025-03-31 12:14:16 +0200

    implement generatePassword

--- src/main/java/ckollmeier/de/PasswordUtility.java
+++ src/main/java/ckollmeier/de/PasswordUtility.java
@@ -1,9 +1,33 @@
 package ckollmeier.de;
 
+import java.util.Random;
+
 public class PasswordUtility {
     public static String generatePassword() {
-        String password = "";
+        StringBuilder password = new StringBuilder();
+        Random rand = new Random();
 
-        return password;
+        // Eight letters, alternatly UC
+        for (int i = 0; i < 4; i++) {
+            String letter = Character.toString((char) (rand.nextInt(26) + 'a'));
+
+            if (i % 2 == 0) {
+                letter = letter.toUpperCase();
+            }
+            password.append(letter);
+        }
+        // three digits replace letters at random positions
+        for (int i = 0; i < 3; i++) {
+            String digit = Character.toString((char) (rand.nextInt(10) + '0'));
+            int position = rand.nextInt(password.length());
+
+            password.insert(position, digit);
+        }
+        // one special character
+        String special = new String[] {"@", "_", "$"}[rand.nextInt(3)];
+        int position = rand.nextInt(password.length());
+        password.insert(position, special);
+
+        return password.toString();
     }
 }
