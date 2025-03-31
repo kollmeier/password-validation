42c3162f018d1cfb9853718f59f3616d8b9c74cf
Carsten Kollmeier <carsten@ckollmeier.de>
2025-03-31 10:24:04 +0200

    implement password length validation

--- src/main/java/ckollmeier/de/PasswordValidation.java
+++ src/main/java/ckollmeier/de/PasswordValidation.java
@@ -3,7 +3,7 @@ package ckollmeier.de;
 public class PasswordValidation {
     public static boolean passwordHasMinEightCharacters(String password) {
         // Implementation
-        return false;
+        return password.length() >= 8;
     }
 
     public static boolean passwordContainsDigits(String password) {
