304eaf2979b4c5d7e9a2132f42b621dbc8545386
Carsten Kollmeier <carsten@ckollmeier.de>
2025-03-31 15:53:40 +0200

    change order of validation steps

--- src/main/java/ckollmeier/de/Main.java
+++ src/main/java/ckollmeier/de/Main.java
@@ -7,14 +7,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input your new password: ");
         String password = scanner.nextLine();
+        if (!PasswordValidation.passwordIsValid(password)) {
+            System.out.println("Password is invalid");
+            return;
+        }
         System.out.println("Please confirm your new password: ");
         String confirmPassword = scanner.nextLine();
         if (!password.equals(confirmPassword)) {
             System.out.println("Passwords do not match");
-            return;
-        }
-        if (!PasswordValidation.passwordIsValid(password)) {
-            System.out.println("Password is invalid");
             return;
         }
         System.out.println("Password valid");
