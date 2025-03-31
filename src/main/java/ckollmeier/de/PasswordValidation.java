5edcdc14345320412913efd5fb73ea04aaa05495
Carsten Kollmeier <carsten@ckollmeier.de>
2025-03-31 10:13:39 +0200

    Method stubs

---
+++ src/main/java/ckollmeier/de/PasswordValidation.java
@@ -0,0 +1,23 @@
+package ckollmeier.de;
+
+public class PasswordValidation {
+    public static boolean passwordHasMinEightCharacters(String password) {
+        // Implementation
+        return false;
+    }
+
+    public static boolean passwordContainsDigits(String password) {
+        // Implementation
+        return false;
+    }
+
+    public static boolean passwordContainsUpperCaseAndLowerCase(String password) {
+        // Implementation
+        return false;
+    }
+
+    public static boolean passwordIsNoCommonPassword(String password) {
+        // Implementation
+        return false;
+    }
+}
